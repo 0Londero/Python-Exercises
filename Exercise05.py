06b882c03a1197c1ff86458e3e314f0013868b37
Otávio Londero <otavio.b.londero@gmail.com>
2025-01-30 21:26:04 -0800

    Added one exercise.

---
+++ Exercise05.py
@@ -0,0 +1,6 @@
+"""
+Uma firma contrata um encanador a R$ 20,00 por dia.
+Escreva um programa que leia o número de dias trabalhados pelo encanador e imprima a quantia liquida que deverá ser paga,
+sabendo-se que são descontados 8% para o imposto de renda.
+"""
+
