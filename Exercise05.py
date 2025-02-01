2ead452b62ed543909935cf514b949c85bf5d1c7
Otávio Londero <otavio.b.londero@gmail.com>
2025-01-31 19:48:23 -0800

    Added one exercise.

--- Exercise05.py
+++ Exercise05.py
@@ -4,3 +4,18 @@ Escreva um programa que leia o número de dias trabalhados pelo encanador e impr
 sabendo-se que são descontados 8% para o imposto de renda.
 """
 
+wage = 20.00
+
+def tax_calculation (wage,days):
+    gross_value = (wage*days)
+    tax = gross_value*(8/100)
+    net_value = gross_value-tax
+    print(f"The Gross wage is: {gross_value}")
+    print(f"The Tax is: {tax}")
+    print(f"The wage after tax is: {net_value:.2f}")
+
+
+print("How much days the worker have worked?")
+days = int(input("Days = "))
+
+tax_calculation(wage,days)
